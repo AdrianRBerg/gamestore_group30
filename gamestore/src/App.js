2fef789da3fc0d4debe04f9c324485df15fc48f4
RobinFHL <rfw0808@gmail.com>
2023-05-03 14:17:27 +0200

    små endringer
    fjernet noen komponent imports her og fikset på knapp

--- gamestore/src/App.js
+++ gamestore/src/App.js
@@ -1,28 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< Updated upstream
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-=======
-import Gameshop from './components/gameshop'
 import favourites from './components/MyFavourites';
 import { Route, Routes } from "react-router-dom"
 import MyFavourites from './components/MyFavourites';
@@ -32,7 +9,5 @@ function App() {
   return (
     <MyFavourites/>
  )
->>>>>>> Stashed changes
-}
 }
 export default App;
--- gamestore/src/components/MyFavourites.js
+++ gamestore/src/components/MyFavourites.js
@@ -1,12 +1,10 @@
 import { useEffect, useState } from "react"
-import Buy from "./buy"
-import GameCard from "./GameCard"
 
 
 export default function MyFavourites(){
 const test = console.log("test")
 return(
-    <button id="button1"> onClick={test}</button>
+    <button id="button1" onClick={test}>test</button>
     )
 
 }
