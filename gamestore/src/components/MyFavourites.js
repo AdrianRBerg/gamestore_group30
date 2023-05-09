c99dfa74829b0800800f720306202a3604bd2cfb
Henriejo2308 <henjo2308@gmail.com>
2023-05-09 13:20:09 +0200

    import React, { useState } from 'react'; import { FavouriteGames } from './FavouriteGames';  const games = [];  function MyFavorites() {   const [favourites, setFavourites] = useState([]);    function addToFavourites(game) {     setFavourites([...favourites, game]);   }    function GameDisplay(props) {     const { game } = props;      function handleAddToFavourites() {       addToFavourites(game);     }      return (       <div>         <h2>{game.title}</h2>         <p>{game.description}</p>         <button onClick={handleAddToFavourites}>Add to favourites</button>       </div>     );   }    return (     <div>       <h1>My favourite games</h1>       {games.map(game => (         <GameDisplay key={game.id} game={game} />       ))}       <FavouriteGames favourites={favourites} />     </div>   ); }  export default MyFavorites;

--- gamestore/src/components/MyFavourites.js
+++ gamestore/src/components/MyFavourites.js
@@ -1,10 +1,40 @@
-import { useEffect, useState } from "react"
+import React, { useState } from 'react';
+import { FavouriteGames } from './FavouriteGames';
 
+const games = [];
 
-export default function MyFavourites(){
-const test = console.log("test")
-return(
-    <button id="button1" onClick={test}>test</button>
-    )
+function MyFavorites() {
+  const [favourites, setFavourites] = useState([]);
 
+  function addToFavourites(game) {
+    setFavourites([...favourites, game]);
+  }
+
+  function GameDisplay(props) {
+    const { game } = props;
+
+    function handleAddToFavourites() {
+      addToFavourites(game);
+    }
+
+    return (
+      <div>
+        <h2>{game.title}</h2>
+        <p>{game.description}</p>
+        <button onClick={handleAddToFavourites}>Add to favourites</button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>My favourite games</h1>
+      {games.map(game => (
+        <GameDisplay key={game.id} game={game} />
+      ))}
+      <FavouriteGames favourites={favourites} />
+    </div>
+  );
 }
+
+export default MyFavorites;
