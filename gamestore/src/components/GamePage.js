dd611deb189091d11682e24c8cdf0e4cc250f46b
Henriejo2308 <henjo2308@gmail.com>
2023-05-13 15:51:15 +0200

    GamePage
    Concat begge arrays for å kunne få listet ut GamePage for spill både i store og mygames

--- gamestore/src/components/GamePage.js
+++ gamestore/src/components/GamePage.js
@@ -1,9 +1,11 @@
 import { useParams } from "react-router-dom"
-import { mygames } from "./games.js"
+import { mygames, store } from "./games.js"
 
 export default function GamePage() {
   const { id } = useParams()
-  const game = mygames.find((game) => game.id === parseInt(id)) //dette er for de spillene vi eier, ikke de i store.
+  const allgames = store.concat(mygames) //concatinerer begge arrayene sammen for å kunne søke i begge
+  const game = allgames.find((game) => game.id === parseInt(id)) //søker i begge arrayene etter spillet
+
 
     return (
         <article className="game-page">
