8365c8eb68c5f79cc7b2057a346937e1b6c70619
Henriejo2308 <henjo2308@gmail.com>
2023-05-13 15:34:41 +0200

    Update Myfavourites.js
    Brukter .filter for å velge ut kun de spillene som er favoritter. Fungerer nå

--- gamestore/src/components/MyFavourites.js
+++ gamestore/src/components/MyFavourites.js
@@ -4,23 +4,23 @@ import { mygames  } from "./games"
 
 
 export default function MyFavourites({}) {
+    const favourites = mygames.filter(game => game.fav === true) //lager en ny array med de spillene som har fav: true, bruker filter for å sortere ut disse 
+                                                                // https://medium.com/poka-techblog/simplify-your-javascript-use-map-reduce-and-filter-bd02c593cc2d
     return (
     <>
     <h1>My Games</h1>
-        {mygames.map(game =>{
-            if(game.fav == true){
-              <>
-              <section key={game.id}>
-              <GameCard
-                  name={game.title}
-                  image={game.img}
-                  genre={game.genres.join(', ')}
-                  link={`/game/${game.id}`}
-              />
-              </section>
-            </>
-            }
-        })} 
-        </>
-    )
-}
+        {favourites.map(game =>( //itererer gjennom favoritter
+            <>
+            <section key={game.id}>
+            <GameCard
+                name={game.title}
+                image={game.img}
+                genre={game.genres.join(', ')}
+                link={`/game/${game.id}`}
+            />
+            </section>
+            </> 
+        ))}
+     </>
+     )
+ }
