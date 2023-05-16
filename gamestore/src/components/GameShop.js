03a97f4d1f9a8d88a7791beed1e9e773a8d37676
RobinFHL <rfw0808@gmail.com>
2023-05-16 17:52:25 +0200

    Update GameShop.js

--- gamestore/src/components/GameShop.js
+++ gamestore/src/components/GameShop.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import { useEffect, useState } from "react"
+import Buy from "./buy"
+import GameCard from "./GameCard"
+import { store } from "./games"
+import Nav from './Nav'
 
-function GameShop() {
-  return (
-    <div>
-      <h1>Game Shop</h1>
-      <p>List of available games goes here</p>
-    </div>
-  );
+export default function Gameshop({}) {
+    
+    return (
+    <>
+    <h1>Gameshop</h1>
+    <Nav/>
+    <h2>For Sale</h2>
+        {store.map(game =>(
+            <>
+            <section className="card" key={game.id}>
+            <GameCard
+                name={game.title}
+                image={game.img}
+                genre={game.genres.join(', ')}
+                link={`/game/${game.slug}`}
+            />
+            <Buy Link={game.link}/>
+            </section>
+            </> 
+        ))} 
+    </>
+    )
 }
-
-export default GameShop;
