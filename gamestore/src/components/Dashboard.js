1c96ec09d9946c942d408506f4d5be13e4cbde46
Henriejo2308 <henjo2308@gmail.com>
2023-05-16 10:02:10 +0200

    lagt til <navbar>

--- gamestore/src/components/Dashboard.js
+++ gamestore/src/components/Dashboard.js
@@ -3,18 +3,22 @@ import "./Dashboard.css"
 import MyGames from './MyGames';
 import MyFavourites from './MyFavourites';
 import Gameshop from './gameshop'
+import { Navbar } from "react-bootstrap";
 
 export default function Dashboard(){ //https://lms.webtricks.blog/kurs/uin/react-navigasjon-og-layout/layout
     return (
         <>
         <div id="card">
+                <Navbar>
+                <h1> MACs Gamehub </h1>
+                </Navbar>
+                <br></br>
+
                 <header>
-                    <h1> MACs Gamehub </h1>
-                    <br></br>
+                     <h2>GAMESHOP</h2>
                 </header>
 
                 <main>
-                    <h2>GAMESHOP</h2>
                     <article className="Game-article1">
                     </article>
                     
